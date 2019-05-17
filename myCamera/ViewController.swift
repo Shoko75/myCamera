89e52cdc05dd9cd40a1c8584b61f866806d81c26
kosho75 <kosho75@gmail.com>
2019-05-16 17:05:42 -0700

    complete tutorial

--- myCamera/ViewController.swift
+++ myCamera/ViewController.swift
@@ -8,13 +8,34 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-
+    @IBOutlet weak var pictureImage: UIImageView!
+    
+    @IBAction func cameraButtonAction(_ sender: Any) {
+        // カメラが利用可能かチェック
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            print("カメラは利用できます")
+            // (1)UIImagePickerControllerのインスタンスを作成
+            let imagePickerController = UIImagePickerController()
+            // (2)sourceTypeにcameraを設定
+            imagePickerController.sourceType = .camera
+            // (3)delegate設置
+            imagePickerController.delegate = self
+            // (4)モーダルビューで表示
+            present(imagePickerController, animated: true, completion: nil)
+            
+        } else {
+            print("カメラは利用できません")
+        }
+    }
+    
+    @IBAction func SNSButtonAction(_ sender: Any) {
+    }
 }
 
